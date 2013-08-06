a18713fad2959e670e355df64c840b79a806d8ab
Charles O'Farrell <cofarrell@atlassian.com>
2013-08-07 07:55:39 +1000

    Upgrade to Stash 2.2. and proper Repository Hooks
    https://answers.atlassian.com/questions/198326/stash-webhook-doesn-t-use-custom-url

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 This is simple example of how to create a plugin for Stash that will trigger a URL webhook in Jenkins.
 
-When running Stash add the following to your command line arguments:
-
-    -Dwebhook.url=http://localhost:8080/jenkins/git/notifyCommit?url=$URL&branches=$BRANCHES
+When running Stash you can enable the webhook for a specific repository via the
+[Repository Hook](https://confluence.atlassian.com/display/STASH/Using+repository+hooks) configuration.
 
 Where $URL will be replaced with the HTTP url of the repository, and $BRANCHES contains a comma separated list of branches.
 
@@ -10,4 +9,4 @@ To build this plugin use the standard Atlassian Plugin SDK and/or run the follow
 
     mvn install
 
-And upload `target/stash-webhook-plugin-1.0.0-SNAPSHOT.jar` to Stash.
+And upload `target/stash-webhook-plugin-2.0.0-SNAPSHOT.jar` to Stash.
--- src/main/java/com/atlassian/stash/plugin/webook/WebHook.java
+++ src/main/java/com/atlassian/stash/plugin/webook/WebHook.java
@@ -1,10 +1,14 @@
 package com.atlassian.stash.plugin.webook;
 
-import com.atlassian.event.api.EventListener;
-import com.atlassian.stash.event.RepositoryPushEvent;
+import com.atlassian.stash.hook.repository.AsyncPostReceiveRepositoryHook;
+import com.atlassian.stash.hook.repository.RepositoryHookContext;
 import com.atlassian.stash.nav.NavBuilder;
 import com.atlassian.stash.repository.RefChange;
 import com.atlassian.stash.repository.RefChangeType;
+import com.atlassian.stash.repository.Repository;
+import com.atlassian.stash.setting.RepositorySettingsValidator;
+import com.atlassian.stash.setting.Settings;
+import com.atlassian.stash.setting.SettingsValidationErrors;
 import com.google.common.base.Function;
 import com.google.common.base.Joiner;
 import com.google.common.base.Predicate;
@@ -16,22 +20,23 @@ import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.util.Collection;
 
-public class WebHook {
+public class WebHook implements AsyncPostReceiveRepositoryHook, RepositorySettingsValidator {
 
-	private static final Logger log = LoggerFactory.getLogger(WebHook.class);
+    private static final Logger log = LoggerFactory.getLogger(WebHook.class);
 	private static final String REFS_HEADS = "refs/heads/";
+    private static final String CONFIG_URL = "url";
 
-	private final NavBuilder navBuilder;
-	private String URL = System.getProperty("webhook.url", "http://localhost:8080/jenkins/git/notifyCommit?url=$URL&branches=$BRANCHES");
+    private final NavBuilder navBuilder;
 
 	public WebHook(NavBuilder navBuilder) {
 		this.navBuilder = navBuilder;
 	}
 
-	@EventListener
-	public void onPushEvent(RepositoryPushEvent event) {
-		String url = getUrl(event);
+    @Override
+    public void postReceive(RepositoryHookContext context, Collection<RefChange> refChanges) {
+		String url = getUrl(context, getUpdateBranches(refChanges));
 		try {
 			URLConnection connection = new URL(url).openConnection();
 			connection.getInputStream().close();
@@ -40,10 +45,10 @@ public class WebHook {
 		}
 	}
 
-	private String getUrl(RepositoryPushEvent event) {
-		String cloneUrl = navBuilder.repo(event.getRepository()).clone("git").buildAbsolute();
-		String branches = urlEncode(Joiner.on(",").join(getUpdateBranches(event)));
-		return URL.replace("$URL", urlEncode(cloneUrl)).replace("$BRANCHES", branches);
+	private String getUrl(RepositoryHookContext context, Iterable<String> b) {
+		String cloneUrl = navBuilder.repo(context.getRepository()).clone(context.getRepository().getScmId()).buildAbsolute();
+		String branches = urlEncode(Joiner.on(",").join(b));
+		return context.getSettings().getString(CONFIG_URL, "").replace("$URL", urlEncode(cloneUrl)).replace("$BRANCHES", branches);
 	}
 
 	private static String urlEncode(String string) {
@@ -54,8 +59,8 @@ public class WebHook {
 		}
 	}
 
-	private Iterable<String> getUpdateBranches(RepositoryPushEvent event) {
-		return Iterables.transform(Iterables.filter(event.getRefChanges(), new Predicate<RefChange>() {
+	private Iterable<String> getUpdateBranches(Collection<RefChange> refChanges) {
+		return Iterables.transform(Iterables.filter(refChanges, new Predicate<RefChange>() {
 			@Override
 			public boolean apply(RefChange input) {
 				// We only care about non-deleted branches
@@ -69,4 +74,12 @@ public class WebHook {
 			}
 		});
 	}
+
+    @Override
+    public void validate(Settings settings, SettingsValidationErrors errors, Repository repository) {
+        String url = settings.getString(CONFIG_URL);
+        if (url == null || url.trim().isEmpty()) {
+            errors.addFieldError(CONFIG_URL, "URL field is blank, please supply one");
+        }
+    }
 }
